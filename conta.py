bab02051fd3e636c3fa2b896a92e2b49d7cc44e2
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-11-17 21:22:29 -0300

    add methods to Conta class
    add sacar, depositar and extrato methods to Conta class.

--- conta.py
+++ conta.py
@@ -5,3 +5,16 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self) -> None:
+        print(f'Saldo de  R$ {self.saldo} do titular {self.titular}')
+
+    def depositar(self, valor) -> None:
+        self.saldo += valor
+
+    def sacar(self, valor) -> None:
+        if valor > self.limite:
+            raise ValueError(f'O limite de saque disponível é: {self.limite}')
+        
+        self.saldo -= valor
+            
