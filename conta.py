d8041c4e7e346a47ac46fcde27f8ba3b8887f4a2
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-11-27 17:36:40 -0300

    private attributes and method transferir.

--- conta.py
+++ conta.py
@@ -1,20 +1,34 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite) -> None:
         print(f'Construindo objeto... {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+	    
+        # Private attributes
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self) -> None:
-        print(f'Saldo de  R$ {self.saldo} do titular {self.titular}')
+        print(f'Saldo de  R$ {self.__saldo} do titular {self.__titular}')
 
     def depositar(self, valor) -> None:
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor) -> None:
-        if valor > self.limite:
-            raise ValueError(f'O limite de saque disponível é: {self.limite}')
-        
-        self.saldo -= valor
-            
+        if valor > self.__limite:
+            raise ValueError(f'O limite de saque disponível é: {self.__limite}')
+
+        self.__saldo -= valor
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    def transferir(self, valor, contaDestino):
+        self.sacar(valor)
+
+        contaDestino.depositar(valor)
+
+        print(f'{self.__titular} transferiu R$ {valor} para {contaDestino.titular}')
+
+
